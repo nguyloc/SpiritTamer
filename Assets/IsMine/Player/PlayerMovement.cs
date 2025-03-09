fceab52202bbf45b678efd58fcc7bf5c0fd05fc0
Tan Loc <locnntps41055@gmail.com>
2025-03-09 21:50:34 +0700

    .

--- Assets/IsMine/Player/PlayerMovement.cs
+++ Assets/IsMine/Player/PlayerMovement.cs
@@ -8,6 +8,8 @@ namespace IsMine.Player
         public float gravity = -9.81f;
         private CharacterController controller;
         private Vector3 velocity;
+        
+        private bool isMoving = false;
 
         void Start()
         {
@@ -16,19 +18,43 @@ namespace IsMine.Player
 
         void Update()
         {
-            float horizontal = Input.GetAxis("Horizontal");
-            float vertical = Input.GetAxis("Vertical");
-
-            Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
-
-            if (direction.magnitude >= 0.1f)
+            Moving();
+        }
+        
+        void Moving()
+        {
+            if (!isMoving)
             {
-                controller.Move(direction * speed * Time.deltaTime);
-            }
+                float horizontal = Input.GetAxis("Horizontal");
+                float vertical = Input.GetAxis("Vertical");
 
-            
-            velocity.y += gravity * Time.deltaTime;
-            controller.Move(velocity * Time.deltaTime);
+                Vector3 direction = new Vector3(horizontal, 0, vertical).normalized; 
+
+                if (direction.magnitude >= 0.1f)
+                {
+                    controller.Move(direction * speed * Time.deltaTime);
+                    
+                    // Check for encounter
+                    CheckForEncounter();
+                }
+                velocity.y += gravity * Time.deltaTime;
+                controller.Move(velocity * Time.deltaTime);
+                
+            }
+        }
+        
+        private void CheckForEncounter()
+        {
+            // Check if player moving on Grass
+            if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, 1f))
+            {
+                if (hit.collider.CompareTag("Grass"))
+                {
+                    // Encounter
+                    isMoving = true;
+                    Debug.Log("Encounter");
+                }
+            }
         }
     }
 }
