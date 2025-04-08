73f418a5604319f4c219f253d33b2f2bc6ba5d7e
Tan Loc <locnntps41055@gmail.com>
2025-04-08 08:33:08 +0700

    Test Play Mode

---
+++ Assets/IsMine/TestPlayMode/TestSource/UnityInputProvider.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace IsMine.TestPlayMode.TestSource
+{
+    public interface IInputProvider
+    {
+        float GetHorizontal();
+        float GetVertical();
+    }
+    
+    public class UnityInputProvider : IInputProvider
+    {
+        public float GetHorizontal() => Input.GetAxis("Horizontal");
+        public float GetVertical() => Input.GetAxis("Vertical");
+    }
+}
